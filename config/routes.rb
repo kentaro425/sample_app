600c3c42fd38c0dc1679d7b842134e40c7890228
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-01-08 10:17:59 +0000

    [Finish]app5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/edit'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'lists/:id' => 'lists#show', as: 'list'
 end
